45b78d85070830fb5bc04fd35a8873c5fb2e6cce
juusolut <jluttine19@student.oulu.fi>
2021-09-02 23:55:06 +0300

    tehtava 1 lähellä valmista

--- exercise1/src/App.js
+++ exercise1/src/App.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header>
       <div className='headerContent'>
-        <div className='logo'><p>HELSINGIN SANOMAT</p></div>
+        <div className='logo'><p>KÄRSÄMÄEN KUULUMISET</p></div>
         <Navigation></Navigation>
         <HeaderButton text='Tilaa' id='order'></HeaderButton>
         <HeaderButton text='Kirjaudu'></HeaderButton>
@@ -37,17 +37,75 @@ const Layout = () => {
   return (
     <div id='canvas'>
       <div id='bannerContainer'>
-        <Banner title='PÄIVÄN TIMANTTI:'></Banner>
-        <Banner title='PÄIVÄN TIMANTTI:'></Banner>
-        <Banner title='MAINOS:'></Banner>
+        <Banner title='PÄIVÄN TIMANTTI:' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit.'></Banner>
+        <Banner title='PÄIVÄN TIMANTTI:' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit.'></Banner>
+        <Banner title='MAINOS:' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit.' isAd='true'></Banner>
       </div>
+      <Main></Main>
+      <Sidebar></Sidebar>
     </div>
   )
 }
 
-const Banner = ({title}) => {
+const Banner = ({title, text, isAd}) => {
+
+  const className = isAd ? 'banner ad': 'banner'
+
   return (
-    <div className='banner'><b>{title}</b></div>
+    <div className={className}><b>{title}</b>{text}</div>
+  )
+}
+
+const Main = () => {
+  return (
+    <main>
+      <article>
+        <h1>Aihealueotsikko</h1>
+        <img src='https://picsum.photos/600/250'></img>
+        <h2>Otsikko</h2>
+      </article>
+    </main>
+  )
+}
+
+const Sidebar = () => {
+
+  const sections = [
+    {
+      id: 0,
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
+    {
+      id: 1,
+      text: 'Esimerkkijuttu'
+    },
+    {
+      id: 2,
+      text: 'Esimerkkijuttu 2 Electric Boogaloo'
+    },
+  ]
+
+  return (
+    <aside>
+      <h3>Luetuimmat</h3>
+      {sections.map( 
+        section => <SidebarSection key={section.id} section={section}></SidebarSection>
+      )}
+    </aside>
+  )
+}
+
+const SidebarSection = ({section}) => {
+
+  console.log(section.id)
+
+  return (
+    <section>
+    <div className='sidebarNumber'> {section.id + 1}
+    </div>
+    <div className='sidebarContent'> {section.text}
+    </div>
+  </section>
   )
 }
 
